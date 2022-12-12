630296f64d875ca6e6cfa46307f0c62a39162e1d
Terry Tompkins <me@terrytompkins.com>
2022-12-12 11:27:04 -0500

    Initial version of notification generated by generator-jhipster@7.9.3

---
+++ notification/src/test/java/com/jhipster/demo/notification/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.jhipster.demo.notification;
+
+import com.jhipster.demo.notification.NotificationApp;
+import com.jhipster.demo.notification.config.AsyncSyncConfiguration;
+import com.jhipster.demo.notification.config.EmbeddedMongo;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { NotificationApp.class, AsyncSyncConfiguration.class })
+@EmbeddedMongo
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
