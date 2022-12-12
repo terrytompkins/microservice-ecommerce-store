d035149c7cfcbcfca1a5ecacdda66b460fa2ea83
Terry Tompkins <me@terrytompkins.com>
2022-12-12 11:27:04 -0500

    Initial version of invoice generated by generator-jhipster@7.9.3

---
+++ invoice/src/test/java/com/jhipster/demo/invoice/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.jhipster.demo.invoice;
+
+import com.jhipster.demo.invoice.InvoiceApp;
+import com.jhipster.demo.invoice.config.AsyncSyncConfiguration;
+import com.jhipster.demo.invoice.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { InvoiceApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
